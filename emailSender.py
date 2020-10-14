bc80265e56099702a9abc7a52c64a6f07a4fba34
loganlach <lwlach@gmail.umd.edu>
2020-10-14 00:06:33 -0400

    added new Java Question implementation

--- emailSender.py
+++ emailSender.py
@@ -2,6 +2,10 @@ import random
 import smtplib
 from pdfminer import high_level as reader
 from selenium import webdriver
+from bs4 import BeautifulSoup
+import lxml
+import requests
+
 PATH = "/Users/loganlach/Developer/chromedriver"
 
 
@@ -10,11 +14,12 @@ PATH = "/Users/loganlach/Developer/chromedriver"
 
 def linAlgQuestions():
     text = getTest('LinearAlgebraTest1')
-    thePacket = reader.extract_text(text)
-    return thePacket
+    return findQuestion(reader.extract_text(text))
 
 def getTest(path):
+    #Pulls a random test from my projects folder
     PATH = '/Users/loganlach/dailyStudyGuideProject/' + path
+    #Gets a random test, out of the three put in there
     todaysTest = random.randint(1, 3)
     options = {1: '/test1.pdf', 2: '/test2.pdf', 3: '/test3.pdf'}
     PATH += options.get(todaysTest)
@@ -22,12 +27,31 @@ def getTest(path):
     text = open(PATH, mode='rb')
     return text
 
+def findQuestion(text, problemConstant, ):
+    constant = 1
+    currentIndex = 0
+    list = []
+    while currentIndex != -1:
+        for i in range(currentIndex, len(text)):
+            currentIndex = text.find('Problem ' + str(constant))
+            list.append(currentIndex)
+            constant += 1
+            break
+    randomQuestion = random.randint(0, len(list) - 1)
+    packet = text[int(list[randomQuestion]):int(list[randomQuestion + 1])]
+    return packet
+
+
 
 def csQuestions():
     text = getTest('CMSC132Questions')
     thePacket = reader.extract_text(text)
 
+#Constant refers to the current problem we are on, like looking for
+#Problem 1, then problem 2, etc.
     constant = 1
+#Current index is self explanatory, it shows which char we are it in relation to the
+#Entire size of the list
     currentIndex = 0
     list = []
     while currentIndex != -1:
@@ -37,17 +61,60 @@ def csQuestions():
             constant += 1
             break
 
-    return thePacket[int(list[0]),int(list[1])]
+    #This part here is for picking a random question out of our available questions
+    randomQuestion = random.randint(0,len(list)-1)
+    packet = thePacket[int(list[randomQuestion]):int(list[randomQuestion+1])]
+    return packet
+
+#Run it from the top, test 2 has a ton of questions and answers included so some unique work has
+#To be done on them
+def javaQuestions():
+    PATH = '/Users/loganlach/dailyStudyGuideProject/JavaQuestions'
+    todaysTest = random.randint(1,2)
+    options = {1: '/test1.pdf', 2: '/test2.pdf'}
+    PATH += options.get(todaysTest)
+    text = reader.extract_text(open(PATH, mode='rb'))
+
+    if todaysTest == 1:
+        constant = 1
+        currentIndex = 0
+        list = []
+        while currentIndex != -1:
+            for i in range(currentIndex, len(text)):
+                currentIndex = text.find(str(constant) + '.')
+                list.append(currentIndex)
+                constant += 1
+                break
+        randomQuestion = random.randint(0, len(list) - 1)
+        packet = text[int(list[randomQuestion]):int(list[randomQuestion + 1])]
+        return packet
+
+    if todaysTest == 2:
+        constant = 1
+        currentIndex = 0
+        list = []
+        while currentIndex != -1:
+            for i in range(currentIndex, len(text)):
+                currentIndex = text.find('Q' + str(constant))
+                list.append(currentIndex)
+                constant += 1
+                break
+
+        questionPick = random.randint(0,len(list)-1)
+        questionAndAnswer = text[int(list[questionPick]): int(list[questionPick + 1])]
+        answerLocation = questionAndAnswer.find('Ans:') + int(list[questionPick])
+        return text[int(list[questionPick]):answerLocation]
 
 
 
 
 
 
-print(csQuestions())
-print('hello')
+
+# C, C, C, A, C
 
 
+print(javaQuestions())
 
 
 
